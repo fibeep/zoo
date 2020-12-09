be30565ebaf15ff58bdb6e907b976f1dee0f81ca
Salo <salomonco@gmail.com>
2020-12-09 14:38:50 -0500

    finished platypus

--- platypus.py
+++ platypus.py
@@ -3,6 +3,9 @@ from random import randint
 from oviparous import Oviparous
 
 class Platypus(Mammal, Oviparous):
+
+    natures_mix_and_match = True
+    __perry_the_platypus = True
     
     def __init__(self, animal_name, sound="Perry the platypus!"):
         super().__init__(animal_name, sound)
@@ -14,12 +17,17 @@ class Platypus(Mammal, Oviparous):
        print(f"{self.name} has layed {babies} eggs although it is a mammal.")
 
     def quiz(self):
-        super().quiz()
-        # Mammal.quiz(self)
-        # Oviparous.quiz(self) -> Check multiple inheritance slides
+        Mammal.quiz(self)
+        Oviparous.quiz(self)
         platypus_game = input("Do you want to awnser questions about platipuses? [Y] [N] ")
         if platypus_game == "Y":
-            # Make platypus quiz
+            response = bool(input('Is the most famous platypus of all time named perry? Respond True or hit enter for False: '))
+            if response == Platypus.__perry_the_platypus:
+                print("Good job!")
+            else:
+                print("Please go watch phineas and pherb")
             pass
         else:
             print("Okay, see you next time!")
+        
+    
